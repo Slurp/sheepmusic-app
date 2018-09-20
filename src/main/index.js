33559054672e9016779f6cd0c8b2ac06f833ddb1
Stephan Langeweg <slurpie@gmail.com>
2018-09-20 21:36:23 +0200

    Testing deploy

--- src/main/index.js
+++ src/main/index.js
@@ -6,7 +6,7 @@ import path from 'path'
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
+  global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
 }
 
 let mainWindow
