d2e90c2a965c445ad8d4c0673a27d0c2649781fc
Stephan Langeweg <slurpie@gmail.com>
2018-10-07 12:24:10 +0200

    More styling tweaks and cleanup

--- src/renderer/components/sidebar/index.vue
+++ src/renderer/components/sidebar/index.vue
@@ -1,43 +1,44 @@
 <template>
-    <div class="nav-drawer-nav">
-        <nav>
-            <ul>
-                <li>
-                    <router-link :to="{name:'recent-albums'}">
-                        <i class="material-icons">album</i><span>Albums</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link :to="{name:'all-artists'}">
-                        <i class="material-icons">recent_actors</i><span>Artists</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link :to="{name:'all-playlists'}">
-                        <i class="material-icons">queue_music</i><span>Playlists</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link :to="{name:'all-genres'}">
-                        <i class="material-icons">receipt</i><span>Genres</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link :to="{name:'user_profile'}">
-                        <i class="material-icons">settings</i><span>You</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link :to="{name:'charts'}">
-                        <i class="material-icons">show_chart</i><span>Donuts and Pies!</span>
-                    </router-link>
-                </li>
-
-
-            </ul>
-        </nav>
-    </div>
-
+    <nav class="nav-drawer-nav">
+        <ul class="drawer-items">
+            <li class="drawer-item">
+                <router-link class="drawer-link" :to="{name:'recent-albums'}">
+                    <i class="drawer-link_icon material-icons">album</i>
+                    <span class="drawer-link_text">Albums</span>
+                </router-link>
+            </li>
+            <li class="drawer-item">
+                <router-link class="drawer-link" :to="{name:'all-artists'}">
+                    <i class="drawer-link_icon material-icons">recent_actors</i>
+                    <span class="drawer-link_text">Artists</span>
+                </router-link>
+            </li>
+            <li class="drawer-item">
+                <router-link class="drawer-link" :to="{name:'all-playlists'}">
+                    <i class="drawer-link_icon material-icons">queue_music</i>
+                    <span class="drawer-link_text">Playlists</span>
+                </router-link>
+            </li>
+            <li class="drawer-item">
+                <router-link class="drawer-link" :to="{name:'all-genres'}">
+                    <i class="drawer-link_icon material-icons">receipt</i>
+                    <span class="drawer-link_text">Genres</span>
+                </router-link>
+            </li>
+            <li class="drawer-item">
+                <router-link class="drawer-link" :to="{name:'user_profile'}">
+                    <i class="drawer-link_icon material-icons">settings</i>
+                    <span class="drawer-link_text">You</span>
+                </router-link>
+            </li>
+            <li class="drawer-item">
+                <router-link class="drawer-link" :to="{name:'charts'}">
+                    <i class="drawer-link_icon material-icons">show_chart</i>
+                    <span class="drawer-link_text">Donuts and Pies!</span>
+                </router-link>
+            </li>
+        </ul>
+    </nav>
 </template>
 <script>
   export default {}
